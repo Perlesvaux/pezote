801842a7235d4f99acabd3a9210334367407e1ef
chico <99514791+Perlesvaux@users.noreply.github.com>
2024-05-10 16:38:58 -0600

    Added outputFormat field. TODO: update frontend.

--- index.js
+++ index.js
@@ -40,54 +40,48 @@ app.post("/ask", async(req, res)=>{
   } catch (error) {
     console.error(error)
     res.send('')
-    
   }
 
 });
 
 
-
+// Form has a 'outputFormat'field. User selects 'Original', 'mp3', etc.
 // Form has a 'path2video' field. This is where video URL goes.
 // when user submits the POST-request, the server responds with a file.
 app.post("/",  MWLogger, async(req, res) => {
 
-  // const metadata = axios.get(req.body.path2video)
-    // .then(response => {
-    //   const html = response.data;
-    //   const $ = cheerio.load(html);
-    //   const title = $('title').text();
-    //   return title;
-    // })
+    // Early exit if invalid format
+    if (!['original', 'mp3'].includes(req.body.outputFormat)) return res.end();
+
+    // res.setHeader('Content-Type', 'audio/mp3');
+    // res.setHeader('Content-Disposition', `attachment; filename="romulo"`);
+    res.setHeader('Content-Disposition', `attachment`);
+
+    const cmd = 'yt-dlp';
+    let args = ['-q', '-o', '-', `${req.body.path2video}`];
+    if (req.body.outputFormat!='original') args = [...args, '|', 'ffmpeg', '-i', 'pipe:0', '-f', `${req.body.outputFormat}`, 'pipe:1'];
 
 
-  // const metadata = await axios.get(req.body.path2video)
-  //   .then(response => {
-  //     const html = response.data;
-  //     const $ = cheerio.load(html);
-  //     const title = $('title').text();
-  //     return title;
-  //   })
-  // console.log(metadata);
-
-    res.setHeader('Content-Type', 'audio/mp3');
-    res.setHeader('Content-Disposition', `attachment; filename="romulo"`);
 
     // Convert to mp3
     // const process = spawn('yt-dlp', ['-q', '-o', '-', `${req.body.path2video}`, '|', 'ffmpeg', '-i', 'pipe:0', '-f', 'mp3', 'pipe:1'], {shell:true})
   
     // Download original file
-    const process = spawn('yt-dlp', ['-q', '-o', '-', `${req.body.path2video}`])
+    // const process = spawn('yt-dlp', ['-q', '-o', '-', `${req.body.path2video}`])
+    
+    const process = spawn(cmd, args, {shell:true});
 
     process.stdout.on('data', (data)=>{
       res.write(data);
     })
-    //
+
     process.on('close', (code)=>{
       if (code!== 0) console.error(`LONG PIPE process exited with code: ${code}`);
       res.end()
     })
 
 });
+
 
 
 //App is ready to go!
